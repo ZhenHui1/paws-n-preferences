0c30592a2399c33637456696156e47de8edcc1b0
ZhenHui1 <93213863+ZhenHui1@users.noreply.github.com>
2026-01-11 18:15:16 +0800

    feat: update site branding and finalize summary view

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,202 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
+import { useEffect, useState } from "react";
+import CatCard from "./components/CatCard";
+import { getCats, type Cat } from "@/lib/cat-api";
+import PawBackground from "./components/PawBackground";
 
 export default function Home() {
+  const [cats, setCats] = useState<Cat[]>([]);  // List of cats to swipe through
+  const [likedCats, setLikedCats] = useState<Cat[]>([]); // Cats the user liked
+  const [cardIndex, setCardIndex] = useState(0);  // Current position in the cat deck
+  const [loading, setLoading] = useState(true); // Loading state
+
+  // Fetch new batch of cats
+  const fetchNewCats = async () => {
+    setLoading(true);
+    try {
+      const data = await getCats(10);
+      setCats(data);    // Update cat list
+      setCardIndex(0);  // Reset position
+      setLikedCats([]); // Clear liked cats
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+  // CALL API: Instant Load Server Component - Fetch cats on mount
+  useEffect(() => {
+    fetchNewCats();
+  }, []);
+
+  // Handle swipe action
+  function handleSwipe(direction: "left" | "right", cat: Cat) {
+    // If swiped right, add to "favourites" list
+    if (direction === "right") setLikedCats((prevList) => [...prevList, cat]);
+    // Move to the next card
+    setCardIndex((i) => i + 1);
+  }
+
+  // Check if all cards have been swiped
+  const finished = cardIndex >= cats.length && !loading;
+
+  // Loading state
+  if (loading)
+    return (
+      <div className="flex h-screen items-center justify-center text-3xl font-bold tracking-wide">
+        Fetching cats... 🐈
+      </div>
+    );
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+    <div className="min-h-screen p-4 sm:p-8 justify-center items-center flex">
+      <PawBackground />
+      <main className="mx-auto w-full portrait:mt-12 portrait:mb-12">
+        <header className="mb-6 justify-between">
+          <h1 className="flex flex-col flex-1 items-center text-3xl font-bold text-neutral-500">
+            Paws & Preferences
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+          <h1 className="flex flex-col flex-1 items-center text-xl font-semibold tracking-tight text-neutral-500">
+            Find Your Favourite Kitty!
+          </h1>
+
+          {!finished && (
+            <div className="flex flex-col flex-1 items-center text-sm text-neutral-600">
+              <span className="bg-neutral-400/20 px-5 py-2 rounded-full mt-2">
+                {cardIndex + 1} / {cats.length}
+              </span>
+            </div>
+          )}
+        </header>
+
+        {!finished ? (
+          <div className="relative h-[clamp(400px,65vh,800px)] w-full">
+            {/* Loop through the cat deck */}
+            {cats.map((cat, currentIndex) => {
+              // Safety check: Ignore cats without IDs
+              if (!cat?.id) return null;
+
+              /**
+               * Only put a few cards in the DOM at a time for performance
+               * - Active Card: The one the user sees, cardIndex (1ST)
+               * - Next Card: Peeking out from underneath, cardIndex + 1 (2ND)
+               * - Preload: Hidden cards loading images in the background (3RD 4TH 5TH)
+               */
+              const isVisible = currentIndex >= cardIndex && currentIndex <= cardIndex + 1;
+              const isPreloading = currentIndex > cardIndex + 1 && currentIndex <= cardIndex + 4;
+              // If NOT ACTIVE (isVisible) or NOT PRELOADING (isPreloading), skip rendering
+              if (!isVisible && !isPreloading) return null;
+
+              return (
+                <div
+                  key={cat.id}
+                  className={
+                    isPreloading ? "invisible pointer-events-none" : "block"
+                  }
+                  aria-hidden={isPreloading}
+                >
+                  <CatCard
+                    card={cat}
+                    isTop={currentIndex === cardIndex}  // The very top card is the only one swipeable
+                    onSwipe={(direction) => handleSwipe(direction, cat)}
+                    zIndex={cats.length - currentIndex} // Higher = lower in the stack
+                  />
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <section className="space-y-6 landscape:space-y-0">
+            {likedCats.length !== 0 && (
+              <div className="rounded-lg bg-white p-6 sm:mb-6 max-w-100 block mx-auto shadow text-center hover:shadow-md transition-shadow">
+                <h2 className="text-2xl font-semibold text">
+                  🐾&nbsp;Purr-fect match!&nbsp;🐾
+                </h2>
+                <p className="text-md text-zinc-600">
+                  You liked {likedCats.length} cat(s).
+                </p>
+              </div>
+            )}
+
+            {likedCats.length === 0 && (
+              <div className="rounded-lg bg-white p-6 sm:mb-6 max-w-100 block mx-auto shadow text-center hover:shadow-md transition-shadow">
+                <h2 className="text-2xl text-zinc-600">
+                  No kitties fetched. 😿
+                </h2>
+                <p className="text-lg text-zinc-600">Wanna try again?</p>
+              </div>
+            )}
+
+            <div className="columns-2 gap-3 space-y-3">
+              {likedCats.map((cat) => (
+                <div
+                  key={cat.id}
+                  className="break-inside-avoid overflow-hidden rounded-lg bg-white shadow-sm border border-neutral-100"
+                >
+                  <img
+                    src={cat.url}
+                    alt={`cat-${cat.id}`}
+                    className="w-full h-auto block" // h-auto to show full aspect ratio
+                  />
+                </div>
+              ))}
+            </div>
+
+            <button
+              className="
+                block mx-auto w-full max-w-60 select-none rounded-full 
+                bg-rose-400 px-6 py-4 text-white text-xl font-bold tracking-wide 
+                /* 3D Shadow Effect */
+                shadow-[0_8px_0_0_#be123c] 
+                
+                /* Mobile-specific: Pressing down makes it squish */
+                active:translate-y-2 active:shadow-none active:bg-rose-500
+                
+                /* Desktop-only: Hover effects only work on devices that support hover */
+                @media(hover:hover){hover:shadow-[0_4px_0_0_#be123c] hover:translate-y-1 hover:bg-rose-300}
+                
+                transition-all duration-150 group
+              "
+              onClick={() => {
+                setCardIndex(0);  // Reset position
+                setLikedCats([]); // Clear liked cats
+              }}
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+              <span className="flex items-center justify-center gap-2">
+                Try Again
+                <span className="transition-transform duration-300 group-active:rotate-20 group-active:scale-110 @media(hover:hover){group-hover:rotate-[20deg] group-hover:scale-110}">
+                  🐱
+                </span>
+              </span>
+            </button>
+            <button
+              className="
+                block mx-auto w-full max-w-60 select-none rounded-full 
+                bg-sky-400 px-6 py-4 text-white text-xl font-bold tracking-wide 
+                /* 3D Shadow Effect */
+                shadow-[0_8px_0_0_#0369a1] 
+                
+                /* Mobile-specific: Pressing down makes it squish */
+                active:translate-y-2 active:shadow-none active:bg-sky-500
+                
+                /* Desktop-only: Hover effects only work on devices that support hover */
+                @media(hover:hover){hover:shadow-[0_4px_0_0_#0369a1] hover:translate-y-1 hover:bg-sky-300}
+                
+                transition-all duration-150 group
+              "
+              onClick={fetchNewCats}
             >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+              <span className="flex items-center justify-center gap-2">
+                Start New
+                <span className="transition-transform duration-300 group-active:rotate-20 group-active:scale-110 @media(hover:hover){group-hover:rotate-[20deg] group-hover:scale-110}">
+                  🔃
+                </span>
+              </span>
+            </button>
+          </section>
+        )}
       </main>
     </div>
   );
